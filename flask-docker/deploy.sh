6b8d823b3e11e5e29c44919cbd644052d4551fb3
Raz-Dahan <razdahan31@gmail.com>
2023-07-03 09:30:43 +0300

    Add variables

--- flask-docker/deploy.sh
+++ flask-docker/deploy.sh
@@ -8,7 +8,7 @@ usage() {
 }
 
 run_tests() {
-  /bin/bash /var/lib/jenkins/workspace/flask-docker-pipeline/pipelines/flask-docker/tests.sh
+  /bin/bash /var/lib/jenkins/workspace/Pipeline_Path/tests.sh
 }
 
 # command line options
@@ -24,16 +24,20 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-# Check if all required arguments are provided, if not prase usage info
+# Checking if all required arguments are provided, if not prase usage info
 if [ $# -lt 2 ]; then
   usage
 fi
 
 INSTANCE_IP=$1
 DOCKER_BUILD=$2
+# Change the path to your pipeline
+Pipeline_Path="flask-docker-pipeline/pipelines/flask-docker"
+# Change the key to your aws rsa key pair
+RSA_Key="raz-key.pem"
 
 # Dependencies and Deployment
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/raz-key.pem ec2-user@${INSTANCE_IP} "
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/RSA_Key ec2-user@${INSTANCE_IP} "
 sudo yum update -y
 sudo yum install docker -y
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -53,9 +57,9 @@ fi
 "
 
 echo 'Copying docker-compose.yml and .env to instance...'
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/raz-key.pem /var/lib/jenkins/workspace/flask-docker-pipeline/pipelines/flask-docker/alpaca-flask/docker-compose.yml /var/lib/jenkins/workspace/flask-docker-pipeline/flask-docker/alpaca-flask/get-ver.sh ec2-user@${INSTANCE_IP}:/home/ec2-user
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/RSA_Key /var/lib/jenkins/workspace/Pipeline_Path/alpaca-flask/docker-compose.yml /var/lib/jenkins/workspace/Pipeline_Path/alpaca-flask/get-ver.sh ec2-user@${INSTANCE_IP}:/home/ec2-user
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/raz-key.pem ec2-user@${INSTANCE_IP} "
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/RSA_Key ec2-user@${INSTANCE_IP} "
 echo 'Pulling image from Docker Hub to instance...'
 sudo docker pull ${DOCKER_BUILD}
 echo 'Getting .env file...'
