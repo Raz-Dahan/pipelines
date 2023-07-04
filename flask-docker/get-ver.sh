2a95333bfb1e1dfbaea98677bdd65ceb9a367401
Raz-Dahan <razdahan31@gmail.com>
2023-07-04 17:15:53 +0300

    Restore

--- flask-docker/get-ver.sh
+++ flask-docker/get-ver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NAME='VER='
 TAG=$(curl -s "https://hub.docker.com/v2/repositories/razdahan31/flask-docker/tags" | jq -r '.results[0].name')
-echo $NAME$TAG > ver.env
+echo $NAME$TAG > .env
