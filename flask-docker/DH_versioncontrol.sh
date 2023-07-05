8750d61fec46827c76b44be08280da9d0f3f7e12
Raz-Dahan <razdahan31@gmail.com>
2023-07-05 20:05:47 +0300

    Update

--- flask-docker/DH_versioncontrol.sh
+++ flask-docker/DH_versioncontrol.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+if [[ -f .env ]]; then
+  export $(cat .env | grep -v '^#' | xargs)
+fi
+
 repository=$1
 
-tagsJson=$(curl -sX GET "https://registry.hub.docker.com/v2/repositories/${repository}/tags?page_size=100" | jq -r '.results[].name')
+tagsJson=$(curl -sX GET -u "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "https://registry.hub.docker.com/v2/repositories/${repository}/tags?page_size=100" | jq -r '.results[].name')
 
 readarray -t tags <<< "$tagsJson"
 
 if [ ${#tags[@]} -gt 10 ]; then
   sortedTags=($(printf '%s\n' "${tags[@]}" | sort))
   oldestTag=${sortedTags[0]}
-  docker rmi "${repository}:${oldestTag}"
+  digest=$(curl -sI -u "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' "https://registry.hub.docker.com/v2/${repository}/manifests/${oldestTag}" | awk '/Docker-Content-Digest/ {print $2}' | tr -d $'\r')
+  curl -X DELETE -u "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "https://registry.hub.docker.com/v2/${repository}/manifests/${digest}"
 fi
