c8fcb29253dc05a41e665d500926cdf533ba3e68
Raz-Dahan <razdahan31@gmail.com>
2023-07-05 20:12:37 +0300

    Update

--- flask-docker/DH_versioncontrol.sh
+++ flask-docker/DH_versioncontrol.sh
@@ -10,9 +10,10 @@ tagsJson=$(curl -sX GET -u "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "https://regi
 
 readarray -t tags <<< "$tagsJson"
 
-if [ ${#tags[@]} -gt 10 ]; then
+if [ ${#tags[@]} -gt 9 ]; then
   sortedTags=($(printf '%s\n' "${tags[@]}" | sort))
   oldestTag=${sortedTags[0]}
   digest=$(curl -sI -u "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' "https://registry.hub.docker.com/v2/${repository}/manifests/${oldestTag}" | awk '/Docker-Content-Digest/ {print $2}' | tr -d $'\r')
   curl -X DELETE -u "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "https://registry.hub.docker.com/v2/${repository}/manifests/${digest}"
+  echo "Image $repository:$oldestTag removed from Docker Hub"
 fi
