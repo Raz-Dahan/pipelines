8740f142d4d1b80c458f2c9b288f1a73d292f63f
Raz-Dahan <razdahan31@gmail.com>
2023-07-07 16:15:25 +0300

    test

--- flask-docker/get-ver.sh
+++ flask-docker/get-ver.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt install jq -y
 NAME='VER='
 TAG=$(curl -s "https://hub.docker.com/v2/repositories/razdahan31/flask-docker/tags" | jq -r '.results[0].name')
 echo $NAME$TAG > .env
