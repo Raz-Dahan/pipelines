8e7f3c857cd6085845f2d17e66967a3b64d166a7
Raz-Dahan <razdahan31@gmail.com>
2023-07-11 15:27:22 +0300

    fix

--- flask-kubernetes/DH_versioncontrol.sh
+++ flask-kubernetes/DH_versioncontrol.sh
@@ -6,7 +6,7 @@ REPO=$(echo "$REPOSITORY" | cut -d '/' -f2)
 
 TAGS_JSON=$(curl -sX GET https://registry.hub.docker.com/v2/repositories/$REPOSITORY/tags?page_size=100 | jq -r '.results[].name')
 TAGS_SUM=$(echo $TAGS_JSON | wc -w)
-HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"razdahan31\", \"password\": \"Raz324831890\"}" https://hub.docker.com/v2/users/login/ | jq -r .token)
+HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"${DOCKER_USERNAME}\", \"password\": \"${DOCKER_PASSWORD}\"}" https://hub.docker.com/v2/users/login/ | jq -r .token)
 
 if [[ $TAGS_SUM -gt 10 ]]; then
   OLDEST_TAG=$(echo $TAGS_JSON | awk '{print $NF}')
