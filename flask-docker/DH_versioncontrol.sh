7f999f227eee11066d5bf63b61ad35cc60d18534
Raz-Dahan <razdahan31@gmail.com>
2023-07-11 18:14:23 +0300

    Update

--- flask-docker/DH_versioncontrol.sh
+++ flask-docker/DH_versioncontrol.sh
@@ -4,9 +4,9 @@ REPOSITORY=$1
 USER=$(echo "$REPOSITORY" | cut -d '/' -f1)
 REPO=$(echo "$REPOSITORY" | cut -d '/' -f2)
 
-TAGS_JSON=$(curl -sX GET https://registry.hub.docker.com/v2/repositories/$REPOSITORY/tags?page_size=100 | jq -r '.results[].name')
+TAGS_JSON=$(curl -sX GET https://registry.hub.docker.com/v2/repositories/razdahan31/flask-docker/tags?page_size=100 | jq -r '.results[].name')
 TAGS_SUM=$(echo $TAGS_JSON | wc -w)
-HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"${DOCKER_USERNAME}\", \"password\": \"${DOCKER_PASSWORD}\"}" https://hub.docker.com/v2/users/login/ | jq -r .token)
+HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"razdahan31\", \"password\": \"Raz324831890\"}" https://hub.docker.com/v2/users/login/ | jq -r .token)
 
 if [[ $TAGS_SUM -gt 10 ]]; then
   OLDEST_TAG=$(echo $TAGS_JSON | awk '{print $NF}')
