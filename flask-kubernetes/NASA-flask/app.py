25b7f5b4b9df36207e2506a75cc54b1c7483d5e7
Raz-Dahan <razdahan31@gmail.com>
2023-07-12 10:06:14 +0300

    Update

--- flask-kubernetes/NASA-flask/app.py
+++ flask-kubernetes/NASA-flask/app.py
@@ -21,9 +21,11 @@ def index():
     if request.method == 'POST':
         request_date = request.form['date']
         image_url = get_nasa_image(request_date)['url']
-        return render_template('index.html', image_url=image_url, today_image_url=today_data['url'],today_describe_url=today_data['title'], logo_url=logo_url, icon_url=icon_url, count=int(count))
+        return render_template('index.html', image_url=image_url, today_image_url=today_data['url'],
+                               today_describe_url=today_data['title'], logo_url=logo_url, icon_url=icon_url, count=int(count))
     else:
-        return render_template('index.html', today_image_url=today_data['url'],today_describe_url=today_data['title'], logo_url=logo_url, icon_url=icon_url, count=int(count)) 
+        return render_template('index.html', today_image_url=today_data['url'], today_describe_url=today_data['title'],
+                               logo_url=logo_url, icon_url=icon_url, count=int(count))
 
 def get_nasa_image(date):
     secret_key = os.getenv('API_KEY')
