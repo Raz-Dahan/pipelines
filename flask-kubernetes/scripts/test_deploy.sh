7057558fc89e224a2e5927fed0eacdabd98ec81e
Raz-Dahan <razdahan31@gmail.com>
2023-07-16 14:32:19 +0300

    Update

--- flask-kubernetes/scripts/test_deploy.sh
+++ flask-kubernetes/scripts/test_deploy.sh
@@ -18,7 +18,7 @@ else
 fi
 if ! command -v docker-compose &> /dev/null; then
     echo 'Docker Compose is not installed. Installing Docker Compose...'
-    sudo curl -L 'https://github.com/docker/compose/releases/latest/download/docker-compose-\$(uname -s)-\$(uname -m)' -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     echo 'Docker Compose has been installed.'
 else
