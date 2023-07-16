d7aae11d683ed345a50c7678cd3c97ce15b0f624
Raz-Dahan <razdahan31@gmail.com>
2023-07-16 20:57:42 +0300

    Update

--- flask-kubernetes/scripts/get-ver.sh
+++ flask-kubernetes/scripts/get-ver.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sudo apt install jq -y
+
+if ! command -v jq &> /dev/null; then
+    echo 'jq is not installed. Installing jq...'
+    sudo apt install jq -y
+    echo 'jq has been installed.'
+else
+    echo 'jq is already installed.'
+fi
+    
 NAME='VER='
 REPO='razdahan31/flask-k8s'
 TAG=$(curl -s "https://hub.docker.com/v2/repositories/${REPO}/tags" | jq -r '.results[0].name')
