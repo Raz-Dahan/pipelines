989086f5334c944e33de3812a855185f7ac67184
Raz-Dahan <razdahan31@gmail.com>
2023-07-17 00:07:21 +0300

    Update

--- flask-kubernetes/scripts/deploy.sh
+++ flask-kubernetes/scripts/deploy.sh
@@ -3,10 +3,12 @@
 cd /var/lib/jenkins/workspace/${Pipeline_Path}/scripts
 echo 'Getting .env file...'
 bash get-ver.sh
+export $(cat .env | xargs)
 
 IS_APPLIED=$(kubectl get -f manifest.yaml --ignore-not-found)
 
 if [[ -z "$IS_APPLIED" ]]; then
+    echo "Appling maifest.yaml in the cluster..."
     kubectl apply -f manifest.yaml
     exit 0
 else
@@ -16,10 +18,9 @@ fi
 GET_IMAGE_TAG=$(kubectl describe deployment.apps/flask-deployment | grep -i "image:" | awk '{split($2, a, ":"); print a[2]}')
 
 if [[ "$GET_IMAGE_TAG" == "$VER" ]]; then
+    echo "Setting new image tag=$VER on the deployment..."
     kubectl set image deployment.apps/flask-deployment flask=razdahan31/flask-k8s:$VER
     exit 0
+else
+    echo "Newest image is already set."
 fi
-
-env
-pwd
-ls -la
