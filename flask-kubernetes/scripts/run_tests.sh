de02d6bb3e35e6b94af3b0ab41313860b30eb68b
Raz-Dahan <razdahan31@gmail.com>
2023-07-16 17:54:10 +0300

    Update

--- flask-kubernetes/scripts/run_tests.sh
+++ flask-kubernetes/scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTANCE_IP=$1
+INSTANCE_IP = sh(script: "gcloud compute instances describe test --zone=us-central1-a --format='get(networkInterfaces[0].accessConfigs[0].natIP)'", returnStdout: true).trim()
 
 # Test the http status
 http_response=$(curl -s -o /dev/null -w "%{http_code}" ${INSTANCE_IP}:80)
@@ -13,7 +13,7 @@ else
 fi
 
 # Test if Redis database responding
-db_response=$(gcloud compute ssh --project=named-signal-392608 --zone=us-central1-a test --ssh-flag="-o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no -- sudo docker exec redis sh -c 'redis-cli ping'")
+db_response=$(gcloud compute ssh --project=named-signal-392608 --zone=us-central1-a test --ssh-flag="-o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no" --command "sudo docker exec redis sh -c 'redis-cli ping'")
 
 if [[ $db_respone == 'PONG' ]]; then
     echo "Redis database returned PONG. Test passed!"
