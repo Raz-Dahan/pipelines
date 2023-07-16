d714cdc898f505e9f99e688586191c1b66e6c187
Raz-Dahan <razdahan31@gmail.com>
2023-07-16 15:37:57 +0300

    Update

--- flask-docker/scripts/get-ver.sh
+++ flask-docker/scripts/get-ver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 NAME='VER='
-REPO='razdahan31/flask-k8s'
+REPO='razdahan31/flask-docker'
 TAG=$(curl -s "https://hub.docker.com/v2/repositories/${REPO}/tags" | jq -r '.results[0].name')
 echo $NAME$TAG > .env
