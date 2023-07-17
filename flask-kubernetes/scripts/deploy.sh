65045434d3bf26a1905359eaa193963c6810bfe0
Raz-Dahan <razdahan31@gmail.com>
2023-07-17 15:36:50 +0300

    Update

--- flask-kubernetes/scripts/deploy.sh
+++ flask-kubernetes/scripts/deploy.sh
@@ -7,8 +7,8 @@ cd /var/lib/jenkins/workspace/${Pipeline_Path}/scripts
 echo 'Getting ConfigMap...'
 bash get_config.sh
 
-kubectl apply -f configmap.yaml
+## kubectl apply -f configmap.yaml
+## kubectl apply -f manifest-test.yaml
 kubectl apply -f manifest.yaml
-
 
 GET_IMAGE_TAG=$(kubectl describe deployment.apps/flask-deployment | grep -i "image:" | awk '{split($2, a, ":"); print a[2]}')
