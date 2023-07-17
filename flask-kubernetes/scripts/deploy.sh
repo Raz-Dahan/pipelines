047e144184440dc02c0d5207f4ee11393661ea4a
Raz-Dahan <razdahan31@gmail.com>
2023-07-17 15:07:01 +0300

    Update

--- flask-kubernetes/scripts/deploy.sh
+++ flask-kubernetes/scripts/deploy.sh
@@ -4,21 +4,11 @@ export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 gcloud container clusters get-credentials nasa-cluster
 
 cd /var/lib/jenkins/workspace/${Pipeline_Path}/scripts
-echo 'Getting .env file...'
+echo 'Getting ConfigMap...'
 bash get_config.sh
-export $(cat .env | xargs)
 
 kubectl apply -f configmap.yaml
-
 kubectl apply -f manifest.yaml
 
 
 GET_IMAGE_TAG=$(kubectl describe deployment.apps/flask-deployment | grep -i "image:" | awk '{split($2, a, ":"); print a[2]}')
-
-if [[ "$GET_IMAGE_TAG" == "$VER" ]]; then
-    echo "Setting new image tag=$VER on the deployment..."
-    kubectl set image deployment.apps/flask-deployment flask=razdahan31/flask-k8s:$VER
-    exit 0
-else
-    echo "Newest image is already set."
-fi
--- flask-kubernetes/scripts/get_config.sh
+++ flask-kubernetes/scripts/get_config.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Get VER variable
-
 if ! command -v jq &> /dev/null; then
     echo 'jq is not installed. Installing jq...'
     sudo apt install jq -y
@@ -9,15 +7,9 @@ if ! command -v jq &> /dev/null; then
 else
     echo 'jq is already installed.'
 fi
-    
-NAME='VER='
+
 REPO='razdahan31/flask-k8s'
 TAG=$(curl -s "https://hub.docker.com/v2/repositories/${REPO}/tags" | jq -r '.results[0].name')
-echo $NAME$TAG > .env
-
-# Get config map
-
-ENV_FILE=".env"
 OUTPUT_FILE="configmap.yaml"
 NAMESPACE="default"
 
@@ -28,9 +20,8 @@ metadata:
   namespace: $NAMESPACE
 data:" > "$OUTPUT_FILE"
 
-while IFS= read -r line; do
-  key=$(echo "$line" | cut -d '=' -f 1)
-  value=$(echo "$line" | cut -d '=' -f 2-)
-  echo "  $key: $value" >> "$OUTPUT_FILE"
-done < "$ENV_FILE"
+key="VER"
+value=$TAG
+echo "  $key: $value" >> "$OUTPUT_FILE"
 
+echo "configmap.yaml created"
