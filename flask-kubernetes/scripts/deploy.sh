03808727255417174b20a2edebc3d201cec5615f
Raz-Dahan <razdahan31@gmail.com>
2023-07-19 15:27:29 +0300

    dynamic version

--- flask-kubernetes/scripts/deploy.sh
+++ flask-kubernetes/scripts/deploy.sh
@@ -3,12 +3,21 @@
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 gcloud container clusters get-credentials nasa-cluster
 
-cd ${Pipeline_Path}
-echo 'Getting ConfigMap...'
-bash scripts/get_config.sh
+#cd ${Pipeline_Path}
+#echo 'Getting ConfigMap...'
+#bash scripts/get_config.sh
+
+
 
 ## kubectl apply -f configmap.yaml
 ## kubectl apply -f manifest-test.yaml
-kubectl apply -f manifest.yaml
+#kubectl apply -f manifest.yaml
 
-GET_IMAGE_TAG=$(kubectl describe deployment.apps/flask-deployment | grep -i "image:" | awk '{split($2, a, ":"); print a[2]}')
+#GET_IMAGE_TAG=$(kubectl describe deployment.apps/flask-deployment | grep -i "image:" | awk '{split($2, a, ":"); print a[2]}')
+
+cd ${Pipeline_Path}/flask-chart
+echo 'Getting values.yaml'
+bash scripts/get_values.sh
+rm flask-chart-0.1.0.tgz
+helm package .
+helm install flask-chart flask-chart-0.1.0.tgz
