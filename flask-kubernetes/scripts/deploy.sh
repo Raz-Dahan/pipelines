2604cfa62f7998b92cadc241d6b7471b93a6be04
Raz-Dahan <razdahan31@gmail.com>
2023-07-20 20:59:06 +0300

    Remove old scripts

--- flask-kubernetes/scripts/deploy.sh
+++ flask-kubernetes/scripts/deploy.sh
@@ -4,15 +4,14 @@ export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 gcloud container clusters get-credentials nasa-cluster
 
 cd ${Pipeline_Path}/flask-chart
-echo 'Getting values.yaml'
-bash ../scripts/get_values.sh
-rm flask-chart-0.1.0.tgz
+echo 'Getting chart yamls...'
+bash ${Pipeline_Path}/scripts/get_chart_yamls.sh
 helm package .
 if helm list | grep -q -i "flask-chart"; then
     echo 'Chart already installed'
-    echo 'Performing update...'
-    helm upgrade flask-chart flask-chart-0.1.0.tgz --reuse-values -f values.yaml
+    echo 'Performing upgrade...'
+    helm upgrade flask-chart flask-chart-1.$BUILD_NUMBER.tgz --reuse-values -f values.yaml
 else
     echo 'Installing the chart...'
-    helm install flask-chart flask-chart-0.1.0.tgz
+    helm install flask-chart flask-chart-1.$BUILD_NUMBER.tgz
 fi
