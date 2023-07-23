f604b01777c8f5cbe2fa92175968f353b60306ac
Raz-Dahan <razdahan31@gmail.com>
2023-07-23 11:31:38 +0300

    Update deploy and skip test

--- flask-kubernetes/scripts/deploy.sh
+++ flask-kubernetes/scripts/deploy.sh
@@ -7,11 +7,16 @@ cd ${Pipeline_Path}/chart
 echo 'Getting chart yamls...'
 bash ${Pipeline_Path}/scripts/get_chart_yamls.sh
 helm package .
-if helm list | grep -q -i "flask-chart"; then
-    echo 'Chart already installed'
-    echo 'Performing upgrade...'
-    helm upgrade flask-chart flask-chart-1.$BUILD_NUMBER.0.tgz --reuse-values -f values.yaml
+if helm test flask-chart; then
+    echo 'Tests passed. Proceeding with upgrade/install...'
+    if helm list | grep -q -i "flask-chart"; then
+        echo 'Chart already installed'
+        echo 'Performing upgrade...'
+        helm upgrade flask-chart flask-chart-1.$BUILD_NUMBER.0.tgz --reuse-values -f values.yaml
+    else
+        echo 'Installing the chart...'
+        helm install flask-chart flask-chart-1.$BUILD_NUMBER.0.tgz
+    fi
 else
-    echo 'Installing the chart...'
-    helm install flask-chart flask-chart-1.$BUILD_NUMBER.0.tgz
+  echo 'Tests failed. Skipping upgrade/install...'
 fi
