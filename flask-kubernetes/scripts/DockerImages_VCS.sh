d056202c410ce061791dd6a76a54f921bd0e51e2
Raz-Dahan <razdahan31@gmail.com>
2023-08-06 15:06:24 +0300

    Update

--- flask-kubernetes/scripts/DockerImages_VCS.sh
+++ flask-kubernetes/scripts/DockerImages_VCS.sh
@@ -5,11 +5,11 @@ IMAGE="flask-ip"
 echo "Performing cleanup on local Docker images..."
 echo "Removing images of '$IMAGE' if there are more than 5..."
 
-BUILDS_SUM=$(sudo docker images "$IMAGE" | tail -n +2 | wc -l)
-OLDEST_BUILD=$(sudo docker images --no-trunc --format '{{.Repository}}:{{.Tag}}' "$IMAGE" | tail -n +2 | sort -V | head -n 1)
+BUILDS_SUM=$(docker images "$IMAGE" | tail -n +2 | wc -l)
+OLDEST_BUILD=$(docker images --no-trunc --format '{{.Repository}}:{{.Tag}}' "$IMAGE" | tail -n +2 | sort -V | head -n 1)
 
 if [ "$BUILDS_SUM" -gt 5 ]; then
-    sudo docker rmi "$OLDEST_BUILD"
+    docker rmi "$OLDEST_BUILD"
     echo "Removed oldest build: $OLDEST_BUILD"
 else
     echo "No need to delete the oldest build for '$IMAGE'. Total image count is less than or equal to 5."
