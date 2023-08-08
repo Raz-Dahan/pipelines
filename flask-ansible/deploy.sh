b07412e36ef387cb6e20f37d4a2919620303aa3d
Raz-Dahan <razdahan31@gmail.com>
2023-08-08 09:38:24 +0300

    Change name

--- flask-ansible/deploy.sh
+++ flask-ansible/deploy.sh
@@ -2,4 +2,4 @@
 sudo yum install python -y
 sudo yum install python-pip -y
 sudo pip install ansible
-ansible-playbook pipelines/flask-jenkins/playbook.yml
+ansible-playbook pipelines/flask-ansible/playbook.yml
