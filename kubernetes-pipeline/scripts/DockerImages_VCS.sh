6fc78d2053667d4adcf41d96d5ff858d5baa55d6
Raz-Dahan <razdahan31@gmail.com>
2023-08-14 12:28:18 +0300

    Update

--- kubernetes-pipeline/scripts/DockerImages_VCS.sh
+++ kubernetes-pipeline/scripts/DockerImages_VCS.sh
@@ -5,7 +5,7 @@ IMAGE=$1
 echo "Performing cleanup on local $IMAGE docker images if there are more than 5..."
 while true; do
   BUILDS_SUM=$(docker images "$IMAGE" | tail -n +2 | wc -l)
-  OLDEST_BUILD=$(docker images --no-trunc --format '{{.Repository}}:{{.Tag}}' "$IMAGE" | tail -n 1)
+  OLDEST_BUILD=$(docker images --no-trunc --format '{{.Repository}}:{{.Tag}}' "$IMAGE" | head -n 1)
   if [ "$BUILDS_SUM" -gt 5 ]; then
     docker rmi "$OLDEST_BUILD"
     echo "Removed oldest build: $OLDEST_BUILD"
